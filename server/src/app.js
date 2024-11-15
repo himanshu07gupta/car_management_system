525879c09bed48847f5558474688ab7e2b4b1659
himanshu07gupta <himanshu29gupta0703@gmail.com>
2024-11-15 14:41:09 +0530

    some backend changes

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,15 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose"
+import dotenv from 'dotenv';
+
 
 const app=express();
 // app.use(cors({
 //     origin:process.env.CORS_ORIGIN,
 //     credentials:true
 // }))
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
